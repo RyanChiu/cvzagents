923b9b285f4ca817a6897a8c7cf77c08c1a2ae09
Anffernee Iverson <anffernee_zhao@hotmail.com>
2012-11-09 16:44:18 +0800

    change the pre-get time for lcd/spc stats driver.
    Signed-off-by: Anffernee Iverson <anffernee_zhao@hotmail.com>

--- app/vendors/lcd_stats.php
+++ app/vendors/lcd_stats.php
@@ -9,8 +9,8 @@ if (($argc - 1) != 1) {//if there is 1 parameter and it must mean a date like '2
 /*
  * the following line will make the whole script exit if date string format is wrong
  */
-$date = __get_remote_date($argv[1], "America/New_York", -1);
-$date_l = __get_remote_date($argv[1], "America/New_York", -1, "America/New_York", true);
+$date = __get_remote_date($argv[1], "America/New_York", -5);
+$date_l = __get_remote_date($argv[1], "America/New_York", -5, "America/New_York", true);
 
 /*get the abbreviation of the site*/
 $abbr = __stats_get_abbr($argv[0]);
