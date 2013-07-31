6aa71656a677e6fc6dc292a1f0f97afe529c0009
Ray Allen <ray@ray-Lenovo-IdeaPad-S400.(none)>
2013-07-31 10:01:08 +0800

    add canal controller to take post from outside.
    add canal controller to take post from outside.
    
    Signed-off-by: Ray Allen <ray@ray-Lenovo-IdeaPad-S400.(none)>

---
+++ app/controllers/canal_controller.php
@@ -0,0 +1,40 @@
+<?php
+App::import('vendor', 'ExtraKits', array('file' => 'extrakits.inc.php'));
+?>
+<?php
+class CanalController extends AppController {
+	var $name = 'Canal';
+	var $uses = array();
+	
+	/**
+	 * overrides
+	 */
+	function beforeFilter() {
+		
+		parent::beforeFilter();
+	}
+	
+	/**
+	 * views
+	 */
+	function index() {
+		$this->layout = "emptylayout";
+		
+		$n = -1;
+		if (isset($_POST['ch'])) {
+			$n = $_POST['ch'];
+		}
+		switch ($n) {
+			case 0:
+			case 1:
+				$s = "from $n, accepted";
+				break;
+			default:
+				$s = "illegal post";
+				break;
+		}
+		$this->set(compact("s"));
+		$this->set(compact("n"));
+	}
+}
+	
---
+++ app/views/canal/index.ctp
@@ -0,0 +1,22 @@
+<div style="margin-bottom:10px;border-top:0;border-left:0;border:right:0;border-bottom:solid blue 1px;width:100%;color:blue;">
+<?php echo $s; ?>
+</div>
+<?php
+$logpath = APP . "tmp" . DS . "canals.log";
+$from = "from canal $n: ";
+$now = new DateTime("now", new DateTimeZone("GMT"));
+$now = " [" . $now->format("Y-m-d H:i:s") . "]\n";
+if (empty($_POST)) {
+	error_log(
+		$from . "Nothing posted here" . $now,
+		3, 
+		$logpath
+	);
+} else {
+	error_log(
+		$from . "\n" . print_r($_POST, true) . $now,
+		3,
+		$logpath 
+	);
+}
+?>
